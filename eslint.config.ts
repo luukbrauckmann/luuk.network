943e8361585fae0d5da66230ed0d9582609d8ab2
Luuk <me@luuk.network>
2025-05-24 22:35:59 +0200

    Add Eslint typescript support

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,23 @@
-import { defineConfig } from "eslint/config";
+import ts from "typescript-eslint";
 import js from "@eslint/js";
 import astro from "eslint-plugin-astro";
 
-export default defineConfig([
+const config = ts.config([
   js.configs.recommended,
+  ts.configs.recommendedTypeChecked,
   astro.configs["flat/recommended"],
-  astro.configs["flat/jsx-a11y-strict"]
+  astro.configs["flat/jsx-a11y-strict"],
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    ignores: ["**.astro/**"]
+  }
 ]);
+
+export default config;
