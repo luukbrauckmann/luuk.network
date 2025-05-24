de8e9b115f23a6aa631602d423e0aac0582c3be8
Luuk <me@luuk.network>
2025-05-24 21:33:59 +0200

    Add Prettier

---
+++ prettier.config.ts
@@ -0,0 +1,16 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "none",
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-css-order'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+export default config;
