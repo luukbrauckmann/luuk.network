78144afaf17778a588e2e4e7af73406bc625ae16
Luuk <me@luuk.network>
2025-06-04 22:47:59 +0200

    Add locale selector and some button styles

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Blocks } from "./Blocks/Blocks.astro";
 export { default as Button } from "./Button/Button.astro";
 export { default as Icon } from "./Icon/Icon.astro";
 export { default as IconsSprite } from "./IconsSprite/IconsSprite.astro";
+export { default as LocaleSelector } from "./LocaleSelector/LocaleSelector.astro";
