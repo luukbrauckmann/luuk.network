4a3592ef4c143e80c748a15b429f1782e73ed858
Luuk <me@luuk.network>
2025-06-06 13:46:13 +0200

    Add Footer component with copyright notice

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Blocks } from "./Blocks/Blocks.astro";
 export { default as Button } from "./Button/Button.astro";
 export { default as DyslexicSwitch } from "./DyslexicSwitch/DyslexicSwitch.astro";
+export { default as Footer } from "./Footer/Footer.astro";
 export { default as Icon } from "./Icon/Icon.astro";
 export { default as IconsSprite } from "./IconsSprite/IconsSprite.astro";
 export { default as LocaleSelector } from "./LocaleSelector/LocaleSelector.astro";
