ad69027950ed487de73d6f768cbff49220c12817
Luuk <me@luuk.network>
2025-06-07 11:53:09 +0200

    Apply consistent styling and improve hero block
    The styling changes include a new DecoratedText component, improved hero block
    layout and responsiveness, and adjusted font size scales.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Blocks } from "./Blocks/Blocks.astro";
 export { default as Button } from "./Button/Button.astro";
+export { default as DecoratedText } from "./DecoratedText/DecoratedText.astro";
 export { default as DyslexicSwitch } from "./DyslexicSwitch/DyslexicSwitch.astro";
 export { default as Footer } from "./Footer/Footer.astro";
 export { default as Header } from "./Header/Header.astro";
