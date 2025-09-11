c0afba9c36540f16524671e82f588980c52eacd5
Luuk <me@luuk.network>
2025-09-11 15:07:34 +0200

    Improve font configuration and update Astro
    The commit improves font configuration by adding fallbacks, Unicode
    ranges, and kerning settings while removing unused font variants and
    updating Astro to 5.13.7.

--- astro.config.ts
+++ astro.config.ts
@@ -36,54 +36,63 @@ export default defineConfig({
           provider: "local",
           name: "Nunito",
           cssVariable: "--font-nunito",
+          fallbacks: ["system-ui", "sans-serif"],
           variants: [
             {
                 weight: 200,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-ExtraLight.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 300,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-Light.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 400,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-Regular.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 500,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-Medium.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 600,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-SemiBold.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 700,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-Bold.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 800,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-ExtraBold.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             },
             {
                 weight: 900,
                 style: "normal",
                 src: ["./src/assets/fonts/Nunito/Nunito-Black.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1",
             }
           ]
         },
@@ -91,128 +100,50 @@ export default defineConfig({
           provider: "local",
           name: "Dash",
           cssVariable: "--font-dash",
+          fallbacks: ["system-ui", "sans-serif"],
           variants: [
             {
                 weight: 100,
                 style: "normal",
                 src: ["./src/assets/fonts/Dash/DashCasualL-Hairline.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1, 'liga' 1, 'clig' 1, 'locl' 1",
+
             },
             {
                 weight: 200,
                 style: "normal",
                 src: ["./src/assets/fonts/Dash/DashCasualL-Thin.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1, 'liga' 1, 'clig' 1, 'locl' 1",
             },
             {
                 weight: 300,
                 style: "normal",
                 src: ["./src/assets/fonts/Dash/DashCasualL-Extralight.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1, 'liga' 1, 'clig' 1, 'locl' 1",
             },
             {
                 weight: 400,
                 style: "normal",
                 src: ["./src/assets/fonts/Dash/DashCasualL-Light.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1, 'liga' 1, 'clig' 1, 'locl' 1",
             },
             {
                 weight: 500,
                 style: "normal",
                 src: ["./src/assets/fonts/Dash/DashCasualL-Regular.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1, 'liga' 1, 'clig' 1, 'locl' 1",
             },
             {
                 weight: 700,
                 style: "normal",
                 src: ["./src/assets/fonts/Dash/DashCasualL-Bold.woff2"],
-                display: 'swap'
-            }
-          ]
-        },
-        {
-          provider: "local",
-          name: "Dash Semiwide",
-          cssVariable: "--font-dash-semiwide",
-          variants: [
-            {
-                weight: 100,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-SemiwideHairline.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 200,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-SemiwideThin.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 300,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-SemiwideExtralight.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 400,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-SemiwideLight.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 500,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-SemiwideRegular.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 700,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-SemiwideBold.woff2"],
-                display: 'swap'
-            }
-          ]
-        },
-        {
-          provider: "local",
-          name: "Dash Wide",
-          cssVariable: "--font-dash-wide",
-          variants: [
-            {
-                weight: 100,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-WideHairline.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 200,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-WideThin.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 300,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-WideExtralight.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 400,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-WideLight.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 500,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-WideRegular.woff2"],
-                display: 'swap'
-            },
-            {
-                weight: 700,
-                style: "normal",
-                src: ["./src/assets/fonts/Dash/DashCasualL-WideBold.woff2"],
-                display: 'swap'
+                unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+                featureSettings: "'kern' 1, 'liga' 1, 'clig' 1, 'locl' 1",
             }
           ]
         }
