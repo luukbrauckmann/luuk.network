8082f20d67954649e7ccf3565fcde9a8831a83b9
Luuk <me@luuk.network>
2025-09-28 12:30:10 +0200

    Enhance i18n with full ICU message format and number/date utils

--- src/i18n/ui.ts
+++
@@ -1,6 +0,0 @@
-export const languages = {
-  en: 'English',
-  nl: 'Nederlands',
-};
-
-export const defaultLang = 'en';
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,8 +1,30 @@
-import { defaultLang } from './ui';
-import translations from "@generated/translations.json"
+import translations from "@generated/translations.json";
 
-export function useTranslations(lang: keyof typeof translations) {
-  return function t(key: keyof typeof translations[typeof defaultLang]) {
-    return translations[lang][key] || translations[defaultLang][key];
-  }
+function convertICUMessageFormat(
+  str: string,
+  values: Record<string, string | number>
+) {
+  return str.replace(/\{(\w+)\}/g, (_, match) =>
+    values[match] !== undefined ? String(values[match]) : `{${match}}`
+  );
+}
+
+export function useTranslations(locale: keyof typeof translations) {
+  return {
+    t: function <K extends keyof (typeof translations)[typeof locale]>(
+      key: K,
+      values?: Record<string, string | number>
+    ) {
+      if (!values) {
+        return translations[locale][key];
+      }
+      return convertICUMessageFormat(translations[locale][key], values);
+    },
+    n: function (value: number, options?: Intl.NumberFormatOptions) {
+      return new Intl.NumberFormat(locale, options).format(value);
+    },
+    d: function (value: Date, options?: Intl.DateTimeFormatOptions) {
+      return new Intl.DateTimeFormat(locale, options).format(value);
+    }
+  };
 }
