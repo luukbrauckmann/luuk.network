0d740040eb391cef09f6cc7c7eaf6700ea6d96f7
Luuk <me@luuk.network>
2025-09-28 12:31:30 +0200

    Format code

--- integrations/translations/index.ts
+++ integrations/translations/index.ts
@@ -4,7 +4,11 @@ import { executeQuery } from "@datocms/cda-client";
 import { join } from "node:path";
 import { mkdir, writeFile } from "node:fs/promises";
 import query from "./AllTranslations.query.graphql?raw";
-import type { AllTranslationsQuery, AllTranslationsQueryVariables, SiteLocale } from "@generated/datocms";
+import type {
+  AllTranslationsQuery,
+  AllTranslationsQueryVariables,
+  SiteLocale
+} from "@generated/datocms";
 
 const { DATOCMS_TOKEN } = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
@@ -14,23 +18,33 @@ export default function translations(): AstroIntegration {
     hooks: {
       "astro:config:setup": async ({ config, logger }) => {
         if (config.i18n) {
-          const translations = (await Promise.all(
-            config.i18n.locales.map(async (locale) => {
-              if (typeof locale === 'string') {
-                const { allTranslations } = await executeQuery<AllTranslationsQuery, AllTranslationsQueryVariables>(
-                  query,
-                  {
+          const translations = (
+            await Promise.all(
+              config.i18n.locales.map(async (locale) => {
+                if (typeof locale === "string") {
+                  const { allTranslations } = await executeQuery<
+                    AllTranslationsQuery,
+                    AllTranslationsQueryVariables
+                  >(query, {
                     token: DATOCMS_TOKEN,
                     variables: {
                       locale: locale as SiteLocale
                     }
-                  }
-                );
+                  });
 
-                return { [locale]: allTranslations.reduce((acc, { key, value }) => ({ ...acc, [key as string]: value }), {}) }
-              }
-            })
-          )).reduce((acc, translation) => ({ ...acc, ...translation }), {});
+                  return {
+                    [locale]: allTranslations.reduce(
+                      (acc, { key, value }) => ({
+                        ...acc,
+                        [key as string]: value
+                      }),
+                      {}
+                    )
+                  };
+                }
+              })
+            )
+          ).reduce((acc, translation) => ({ ...acc, ...translation }), {});
 
           const outputDir = join(config.root.pathname, ".generated");
           await mkdir(outputDir, { recursive: true });
